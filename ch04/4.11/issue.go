f04df03c2215316113a4858573a7f2f60c78fa2d
sh-tatsuno <wasabi.mandarin@gmail.com>
2019-06-12 15:36:02 +0900

    WIP: 4.11

---
+++ ch04/4.11/issue.go
@@ -0,0 +1,18 @@
+package issue
+
+import "time"
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url`
+}
