9d4b6e963c706deb431356ba69d004114bbf86c4
sh-tatsuno <wasabi.mandarin@gmail.com>
2019-10-10 23:38:37 +0900

    ch9-2

---
+++ ch09/ex02/popcount/popcount.go
@@ -0,0 +1,27 @@
+package popcount
+
+import (
+	"sync"
+)
+
+var initpcOnce sync.Once
+
+var pc [256]byte
+
+func initpc() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	initpcOnce.Do(initpc)
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
